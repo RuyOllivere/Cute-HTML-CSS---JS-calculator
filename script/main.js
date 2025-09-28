9d910584d2337104b4e34aa8c4b7c58a32f2ffc6
RuyOllivere <ruylivinginadream@duck.com>
2025-09-28 18:21:19 -0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,55 @@
+// inputText = document.getElementById("input-text");
+// calc = inputText.value;
+
+// function appendDigit(digit) {
+//     inputText.value += digit;
+// }
+
+// function appendOperator(operator) {
+//     inputText.value += operator;
+// }
+
+// function calculateResult() {
+
+    
+
+// }
+
+// ...código existente...
+
+let input = document.getElementById('input-text');
+let lastResult = '';
+let justCalculated = false;
+
+function appendDigit(digit) {
+    if (justCalculated) {
+        input.value = '';
+        justCalculated = false;
+    }
+    input.value += digit;
+}
+
+function appendOperator(operator) {
+    if (justCalculated) {
+        justCalculated = false;
+    }
+    input.value += operator;
+}
+
+function clearDisplay() {
+    input.value = '';
+    lastResult = '';
+    justCalculated = false;
+}
+
+function calculateResult() {
+    try {
+        let result = eval(input.value);
+        input.value = result;
+        lastResult = result;
+        justCalculated = true;
+    } catch (e) {
+        input.value = 'Erro';
+        justCalculated = false;
+    }
+}
